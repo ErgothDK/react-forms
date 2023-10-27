6672b934f95213e8f4ad4fc9b2cc766447291887
ErgothDK <ergothdk@gmail.com>
2023-10-27 14:03:39 -0600

    Added event handlers for inputs using useState

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,47 @@
+import { useState, useRef } from "react";
+
 export default function Login() {
+  const [userInput, setUserInput] = useState({
+    email: "",
+    password: "",
+  });
+
+  function submitHandler(e) {
+    e.preventDefault();
+    console.log(userInput);
+  }
+
+  function inputHandler(e) {
+    setUserInput((prevValues) => ({
+      ...prevValues,
+      [e.target.id]: e.target.value,
+    }));
+  }
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={inputHandler}
+            value={userInput.email}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={inputHandler}
+            value={userInput.password}
+          />
         </div>
       </div>
 
